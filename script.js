80b3f1cea7b3021afdcb5433552e42de04838ee3
Yeidy Levels <ylevels@schools.nyc.gov>
2022-08-19 11:09:54 -0400

    moved js script to appropriate file

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("Hello World");
+
+//randomly returns either 'rock', 'paper', or 'scissors'
+function getComputerChoice () {
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  let computerChoice = "";
+
+  if (randomNumber === 1) {
+    computerChoice = "rock";
+  } else if (randomNumber === 2) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+  return computerChoice;
+}
+
+//plays a single round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
+  let player = playerSelection.toLowerCase();
+  let computer = 
+}
+
+console.log(getComputerChoice());
